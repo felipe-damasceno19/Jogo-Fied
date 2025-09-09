44c8da946c8b1648878db92b766fb109086c6b78
felipe-damasceno19 <felipegpt19@gmail.com>
2025-09-09 14:40:34 -0300

    Alterando GameOver

--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -457,33 +457,14 @@ public class KeyHandler implements KeyListener {
     	        }
     	    }
     	}
-     public void gameOverState(int code) {
-    	 if(code == KeyEvent.VK_W) {
-    		 gp.ui.commandNum--;
-    		 if(gp.ui.commandNum < 0) {
-    			 gp.ui.commandNum = 1;
-    		 }
-    		 gp.playSE(3);
-    	 }
-    	 if(code == KeyEvent.VK_S) {
-    		 gp.ui.commandNum++;
-    		 if(gp.ui.commandNum > 1) {
-    			 gp.ui.commandNum = 0;
-    		 }
-    		 gp.playSE(3);
-    	 }
-    	 if(code == KeyEvent.VK_ENTER) {
-    		 if(gp.ui.commandNum == 0) {
-    			 gp.gameState = gp.playState;
-    			 gp.retry();
-    		 }
-    		 else if(gp.ui.commandNum == 1) {
-    			 gp.gameState = gp.titleState;
-    			 gp.restart();
-    		 }
-    	 }
-     }
      
+     public void gameOverState(int code) {
+    	    if (code == KeyEvent.VK_ENTER) {
+    	    	gp.playSE(3);
+    	        System.exit(0); // Fecha o jogo
+    	    }
+    	}
+
     // Método chamado quando uma tecla é solta
     @Override
     public void keyReleased(KeyEvent e) {
--- src/main/UI.java
+++ src/main/UI.java
@@ -608,46 +608,36 @@ public class UI {
        
     // Desenha Game over
     public void drawGameOverScreen() {
-    	
-    	g2.setColor(new Color (0,0,0,150));
-    	g2.fillRect(0, 0, gp.screenWidth, gp.screenHeight);
-    	
-    	int x;
-    	int y;
-    	String text;
-    	g2.setFont(g2.getFont().deriveFont(Font.BOLD, 110f));
-    	
-    	text = "GAME OVER";
-    	
-    	//SOMBRA
-    	g2.setColor(Color.black);
-    	x = getXforCenteredText(text);
-    	y = gp.tileSize * 4;
-    	g2.drawString(text, x, y);
-    	
-    	//GAME OVER
-    	g2.setColor(Color.white);
-    	g2.drawString(text, x-4, y-4);
-    	
-    	//TENTE NOVAMENTE
-    	g2.setFont(g2.getFont().deriveFont(50f));
-    	text = "TENTE NOVAMENTE";
-    	x = getXforCenteredText(text);
-    	y += gp.tileSize * 4;
-    	g2.drawString(text, x, y);
-    	if(commandNum == 0) {
-    		g2.drawImage(selectorImage, x - gp.tileSize + 27, y - gp.tileSize + 37, 24, 24, null);
-    	}
-    	
-    	//VOLTAR AO MENU
-    	text = " MENU";
-    	x = getXforCenteredText(text);
-    	y += 55;
-    	g2.drawString(text, x, y);
-    	if(commandNum == 1) {
-    		g2.drawImage(selectorImage, x - gp.tileSize + 27, y - gp.tileSize + 37, 24, 24, null);
-    	}
+        g2.setColor(new Color(0,0,0,150));
+        g2.fillRect(0, 0, gp.screenWidth, gp.screenHeight);
+
+        int x;
+        int y;
+        String text;
+        g2.setFont(g2.getFont().deriveFont(Font.BOLD, 110f));
+
+        text = "GAME OVER";
+
+        // SOMBRA
+        g2.setColor(Color.black);
+        x = getXforCenteredText(text);
+        y = gp.tileSize * 4;
+        g2.drawString(text, x, y);
+
+        // GAME OVER
+        g2.setColor(Color.white);
+        g2.drawString(text, x-4, y-4);
+
+        // SAIR 
+        g2.setFont(g2.getFont().deriveFont(50f));
+        text = "SAIR";
+        x = getXforCenteredText(text);
+        y += gp.tileSize * 4;
+        g2.drawString(text, x, y);
+
+        g2.drawImage(selectorImage, x - gp.tileSize + 27, y - gp.tileSize + 37, 24, 24, null);
     }
+
     
     public void drawInteractionPrompt() {
         String text = "Aperte '" + interactKey.toUpperCase() + "' para interagir";
